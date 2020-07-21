295fdcb986df28fed8582b5c42f4c97a91adfc0b
GabrielDCastro <gabriel.rangel12@gmail.com>
2020-07-20 21:11:25 -0300

    Parsing at g1.com.br

---
+++ Parsing.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+url =  'http://g1.com.br'
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                        ' AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/84.0.4147.89 Safari/537.36'}
+
+req = requests.get(url, headers=header)
+html = req.text
+
+
+
+soup = BeautifulSoup(html, 'html.parser')
+
+colection = soup.find_all(class_='feed-post-link gui-color-primary gui-color-hover') #this class is used in every post
+
+for item in colection:
+    print(item.get_text(), "\n")
